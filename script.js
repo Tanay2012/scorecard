edb7f4187db1a34fbc6bca1a7d7ddcba16baa563
Tanay Das <td961381@gmail.com>
2025-11-05 13:45:52 +0530

    nothing change

---
+++ script.js
@@ -0,0 +1,26 @@
+let score1 = 0;
+function increment1home(){
+    score1 += 1;
+    document.getElementById("score1").innerText = score1;
+}
+function increment2home(){
+    score1 += 2;
+    document.getElementById("score1").innerText = score1;
+}
+function increment3home(){
+    score1 += 3;
+    document.getElementById("score1").innerText = score1;
+}
+let score2 = 0;
+function increment1guest(){
+    score2 += 1;
+    document.getElementById("score2").innerText = score2;
+}
+function increment2guest(){
+    score2 += 2;
+    document.getElementById("score2").innerText = score2;
+}
+function increment3guest(){
+    score2 += 3;
+    document.getElementById("score2").innerText = score2;
+}
